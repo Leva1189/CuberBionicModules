f64b1a0e9b4f0a70525289fd1ebe75bf7ccf9651
Dmytro Levchenko <levchenkodm13@gmail.com>
2024-12-18 15:49:35 +0200

    task3.1

--- 3_oop/tasks/task3.1.py
+++ 3_oop/tasks/task3.1.py
@@ -4,13 +4,15 @@
     Створіть кілька різних книжок. Визначте для нього методи _repr_ та _str_.
 '''
 class Book:
-    def __init__(self, author:str, name:str, year:str, genre:str):
+    def __init__(self, author:str, name:str, year:int, genre:str):
         self.author = author
         self.name = name
         self.year = year
         self.genre = genre
 
-book1 = Book("",)
-book2 = Book("",)
-book3 = Book("")
+book1 = Book("Зед Шоу", "Learn Python the Hard Way", 2013, "Training")
+book2 = Book("Олексій Васильєв", "Програмування мовою Python", 2024, "Training")
+book3 = Book("Грегори Девид Робертс", "Шантарам", 2003, "Novel")
+
+print(book1.author)
 
