b1b8a68ab56685c30b80c01c155c8def5ee70bfb
Dmytro Levchenko <levchenkodm13@gmail.com>
2024-12-27 13:33:57 +0200

    add task3.2.2

---
+++ 3_oop/tasks3.2/task3.2.2.py
@@ -0,0 +1,40 @@
+'''Завдання 1
+    Опишіть класи графічного об'єкта, 
+    прямокутника та об'єкта, 
+    який може обробляти натискання миші. 
+    Опишіть клас кнопки. 
+    Створіть об'єкт кнопки та звичайного прямокутника. 
+    Викличте метод натискання на кнопку.       
+'''
+
+class GraphicObject:
+    def click(self):
+        print('Click on the object')
+
+class Rectangle(GraphicObject):
+    def __init__(self, width:int, height:int):
+        self.width = width
+        self.height = height
+
+class Button(Rectangle):
+    def click(self):
+        print('Click on the button')
+
+Button1 = Button(10, 20)
+Rectangle1 = Rectangle(10, 0)
+
+Button1.click()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
